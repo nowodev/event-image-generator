942c190a4823fb859c40fad6c6abd787604c2ca4
Favour <toluwaniokunowo@gmail.com>
2022-02-05 16:23:12 +0100

    add color selector component

---
+++ src/components/CustomColors.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-between pt20">
+    <button class="w-10 h-10 bg-red-500 rounded-full"></button>
+    <button class="w-10 h-10 bg-blue-500 rounded-full"></button>
+    <button class="w-10 h-10 bg-indigo-500 rounded-full"></button>
+    <button class="w-10 h-10 bg-gray-500 rounded-full"></button>
+    <button class="w-10 h-10 bg-green-500 rounded-full"></button>
+    <button class="w-10 h-10 bg-white rounded-full"></button>
+    <button class="w-10 h-10 bg-black rounded-full"></button>
+    <button class="w-10 h-10 bg-cyan-500 rounded-full"></button>
+  </div>
+</template>
