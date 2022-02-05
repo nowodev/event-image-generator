12c475f79e404d990b27fe57ac790914c25a42f4
Favour <toluwaniokunowo@gmail.com>
2022-02-05 23:12:17 +0100

    WIP: load all pages
    Signed-off-by: Favour <toluwaniokunowo@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 import Background from './Layout/Background.vue';
-import Form from './components/Form.vue';
-import CustomColors from './components/CustomColors.vue';
-import Download from './components/Download.vue';
-</script>
+import Home from "./Pages/Home.vue";</script>
 
 <template>
   <Background>
-    <Form />
-    <CustomColors />
-    <Download />
+    <Home />
   </Background>
-  <!-- <img alt="Vue logo" class="mr-auto ml-auto" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />-->
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
-  color: #2c3e50;
+  /*color: #2c3e50;*/
   /* margin-top: 60px; */
 }
 </style>
